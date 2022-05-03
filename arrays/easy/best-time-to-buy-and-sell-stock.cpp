e22e91ab383755ec8dd1bc1bd3a05aeab190f1ee
akashg001 <akash.gupta4701@gmail.com>
2022-05-03 20:11:11 +0530

    best-time-to-buy-and-sell-stock

---
+++ arrays/easy/best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+      int less=prices[0],diff=0;
+     for(int i=0;i<prices.size();i++){
+       if(prices[i]<less){
+         less=prices[i];
+       }
+       if(diff<prices[i]-less){
+         diff=prices[i]-less;
+       }
+       }
+      return diff;
+    }
+};
